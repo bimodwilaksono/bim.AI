3b439f2e0112e70040465ef4ae08e292f91b024c
Bimo Laksono <bimodwilaksono@gmail.com>
2025-04-19 23:08:16 +0700

    Add Not Found Page

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
+  route("*", "routes/404.tsx"),
   route("login", "routes/login.tsx"),
   route("register", "routes/register.tsx"),
   route("forgot-password", "routes/forgot-password.tsx"),
