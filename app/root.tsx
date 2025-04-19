24dd8faa533a86feb422783ecce111bde1f68cfd
Bimo Laksono <bimodwilaksono@gmail.com>
2025-04-19 19:49:18 +0700

    Init React Router v7

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,16 @@
 import {
+  isRouteErrorResponse,
+  Links,
+  Meta,
   Outlet,
-  useLoaderData,
-} from "@remix-run/react";
-import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
 
-import "./tailwind.css";
-import Header from "./components/base/header-main";
-import HeaderUser from "./components/base/header-user";
-import { themeSessionResolver } from "./components/theme/theme.server";
-import { BaseLayout } from "./components/base/base-layout";
+import type { Route } from "./+types/root";
+import "./app.css";
 
-export const links: LinksFunction = () => [
+export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
@@ -23,24 +23,53 @@ export const links: LinksFunction = () => [
   },
 ];
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const { getTheme } = await themeSessionResolver(request);
-
-  return {
-    theme: getTheme()
-  }
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
-  const data = useLoaderData<typeof loader>()
+  return <Outlet />;
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack: string | undefined;
+
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? "404" : "Error";
+    details =
+      error.status === 404
+        ? "The requested page could not be found."
+        : error.statusText || details;
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message;
+    stack = error.stack;
+  }
+
   return (
-    <BaseLayout theme={data.theme}>
-      <div className="flex-col bg-background">
-        <Header>
-          <HeaderUser />
-        </Header>
-        <Outlet />
-      </div>
-    </BaseLayout>
+    <main className="pt-16 p-4 container mx-auto">
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre className="w-full p-4 overflow-x-auto">
+          <code>{stack}</code>
+        </pre>
+      )}
+    </main>
   );
 }
