d136f38cb2552dc9c60a6e8b61df33d664993e7e
Bimo Laksono <bimodwilaksono@gmail.com>
2025-04-29 16:59:34 +0700

    Tes Hono

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { reactRouter } from '@react-router/dev/vite';
+import { reactRouterHonoServer } from 'react-router-hono-server/dev'; // add this
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouterHonoServer(), reactRouter(), tsconfigPaths()],
 });
